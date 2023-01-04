8c79c2724df59aca5318f20aa746092d5dfbdeec
Feli87 <felipe.traina.azul@gmail.com>
2023-01-04 00:33:54 -0300

    update

--- push.js
+++ push.js
@@ -1,12 +1,42 @@
-var push = require('web-push');
+const express = require('express');
+const webpush = require('web-push');
+
+const app = express();
 
 let vapidKeys = {
   publicKey: 'BPanIOg2KCAF4SapeX2y1lewqPYpaXTwIDDr51HHzjCAHmh2_GpeYQS43LeaOf8KkNqp_erull167m2NA9HZU5A',
   privateKey: 'R9jAj_s5AbM0oqko_wq4nXk7-Kbq0hzt1FX1mBlCWR0'
 }
 
-push.setVapidDetails('mailto:test@travtion.com',vapidKeys.publicKey, vapidKeys.privateKey )
+// Set the public and private keys for the push notification
+webpush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
-let sub = {};
+// Set up a route to accept push notification subscriptions
+app.post('/subscribe', (req, res) => {
+  // Save the subscription object received from the client
+  saveSubscription(req.body).then(() => {
+    res.status(201).json({});
+  }).catch((err) => {
+    res.status(500).json({error: err});
+  });
+});
 
-push.sendNotification(sub, "test message")
+// Set up a route to send push notifications
+app.post('/send-notification', (req, res) => {
+  // Get the subscription object
+  getSubscription().then((subscription) => {
+    // Send the push notification
+    webpush.sendNotification(subscription, 'Your Push Notification Message').catch((err) => {
+      console.error(err);
+    });
+  });
+});
+
+// Start the server
+app.listen(8000, () => {
+  console.log('Server listening on port 3000');
+});
